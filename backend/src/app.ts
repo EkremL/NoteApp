32ed2d3d044b27df1f9a5bd4e13677f2df5c86c4
EkremL <ekremlale1907@gmail.com>
2024-06-27 20:55:29 +0300

    improved error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 // import NoteModel from "./models/note";
 import notesRoutes from "./routes/notes";
 import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 //morganı import edince hata vericek cunku typescript projesi icin npm i --save-dev @types/morgan de kurmamız gerek
 
 const app = express();
@@ -28,19 +29,41 @@ app.use("/api/notes", notesRoutes);
 // );
 
 //!app.get in altına, error handlerin üstüne yazmak önemli!
+// app.use((req, res, next) => {
+//   //burada tekrar type belirtmedik, aşağıdan otomatik inferred(çıkarım) edildi.
+//   //Eğer belirtirsek compiler hata vericek.
+//   next(Error("Endpoint Not Found"));
+// });
+
+//?HTTP-ERRORS PACKAGE ile error handlingi daha gelişmiş hale getiriyoruz
 app.use((req, res, next) => {
   //burada tekrar type belirtmedik, aşağıdan otomatik inferred(çıkarım) edildi.
   //Eğer belirtirsek compiler hata vericek.
-  next(Error("Endpoint Not Found"));
+  //!createHttpError, 2 adet parametre almaktadır, 1. si  belirttiğimiz statüs kodu, 2. si mesajımız
+  next(createHttpError(404, "Endpoint Not Found"));
 });
 
 //! error handling in typescript
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+//   console.error(error);
+//   let errorMessage = "An unknown error occurred";
+//   if (error instanceof Error) errorMessage = error.message;
+//   res.status(500).json({ error: errorMessage });
+// });
+//!HTTP ERRORS Code Upgrade
+//eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = "An unknown error occurred";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    //! buradaki isHttpError http paketinden gelmektedir.
+    //*buradaki error.status ve error.message, http paketinden gelmektedir.
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import NoteModel from "../models/note";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 //! Get Notes
 export const getNotes: RequestHandler = async (req, res, next) => {
@@ -12,11 +14,28 @@ export const getNotes: RequestHandler = async (req, res, next) => {
   }
 };
 
+//!Olusturacağımız notun icindeki verilerin tipini belirtmemiz gerek bunun icin interface olusturduk
+
+interface CreateNoteBody {
+  title?: string; //zorunlu olmasına rağmen endpointe istek atıldığında verinin gönderileceğinden emin olmak icin question mark koyduk
+  text?: string; //modelde zorunlu tutmadığımız icin question mark koyduk
+}
+
 //! Create Note
-export const createNote: RequestHandler = async (req, res, next) => {
+export const createNote: RequestHandler<
+  unknown,
+  unknown,
+  CreateNoteBody,
+  unknown
+> = async (req, res, next) => {
   const title = req.body.title;
   const text = req.body.text;
   try {
+    //title i zorunlu tuttuğumuz icin olmadığı durumda bir hata ekledik
+    if (!title) {
+      throw createHttpError(400, "Note must have a title!");
+    }
+
     const newNote = await NoteModel.create({
       title: title,
       text: text,
@@ -32,7 +51,18 @@ export const createNote: RequestHandler = async (req, res, next) => {
 export const getSingleNote: RequestHandler = async (req, res, next) => {
   const noteId = req.params.noteId;
   try {
+    //! buraya normalde mongonun kabul ettiği 24 haneli idyi kontrol eden bir hata ekledik
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Invalid note id!"); //400 bad request hatası veriyoruz
+    }
+
     const note = await NoteModel.findById(noteId).exec();
+
+    //! buraya da note olmadığı zamanki hatayı ekledik
+    if (!note) {
+      throw createHttpError(404, "Note not found!"); //404 not found hatası veriyoruz
+    }
+
     res.status(200).json(note);
   } catch (error) {
     next(error);
