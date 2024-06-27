b608dcf17a81225df5bcc5146d05b4abe4246733
EkremL <ekremlale1907@gmail.com>
2024-06-27 04:18:00 +0300

    get and post endpoints and routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,32 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import NoteModel from "./models/note";
+// import NoteModel from "./models/note";
+import notesRoutes from "./routes/notes";
+import morgan from "morgan";
+//morganı import edince hata vericek cunku typescript projesi icin npm i --save-dev @types/morgan de kurmamız gerek
 
 const app = express();
 
-app.get("/", async (req, res, next) => {
-  try {
-    // throw Error("BOOM!");
-    const notes = await NoteModel.find().exec();
-    res.status(200).json(notes);
-  } catch (error) {
-    next(error);
-  }
-});
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+//!artık yeni middlewareyi ekliyoruz ve endpointi belirtiyoruz
+app.use("/api/notes", notesRoutes);
+
+// app.get("/", async (req, res, next) => {
+//   try {
+//     // throw Error("BOOM!");
+//     const notes = await NoteModel.find().exec();
+//     res.status(200).json(notes);
+//   } catch (error) {
+//     next(error);
+//   }
+// }
+//bu kısmı controller ve route içine aldık dolayısıyla parametrelerin tipini orada tekrar belirtebiliriz
+// ya da farklı bir yöntem olarak RequestHandler kullanabiliriz böylece parametrelerin type'i inferred edilir
+// );
+
 //!app.get in altına, error handlerin üstüne yazmak önemli!
 app.use((req, res, next) => {
   //burada tekrar type belirtmedik, aşağıdan otomatik inferred(çıkarım) edildi.
---
+++ backend/src/routes/notes.ts
@@ -0,0 +1,23 @@
+import express from "express";
+//controlleri buraya import ediyoruz
+//1. yöntem
+import * as NotesController from "../controllers/notes";
+
+const router = express.Router();
+
+//!get all notes route
+router.get("/", NotesController.getNotes);
+//!create note route
+router.post("/", NotesController.createNote);
+//!get single note route
+router.get("/:noteId", NotesController.getSingleNote);
+
+//1. yöntem (eğer sadece 1 fonksiyon varsa kullanılabilir ama karmaşık ve birden fazla olanlarda yukardaki kullanılır)
+
+//2. yöntem (eğer sadece 1 fonksiyon varsa kullanılabilir ama karmaşık ve birden fazla olanlarda yukardaki kullanılır)
+
+// import { getNotes } from "../controllers/notes";
+// const router = express.Router();
+// router.get("/", getNotes);
+
+export default router;
